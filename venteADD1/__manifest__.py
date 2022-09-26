a82a2b1a586eaea40d79caf4881060982bbd5c27
Issa-IA <80253673+Issa-IA@users.noreply.github.com>
2022-09-26 11:47:38 +0100

    Add files via upload

---
+++ venteADD1/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "venteADD1",
+
+    'summary': """  """,
+
+    'description': """ """,
+
+    'author': "",
+    'website': "",
+
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','sale','contacts','product'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/sale_order_line.xml',
+        'views/partner.xml',
+        'views/product_inherit.xml',
+        'views/serach_advanced_partner.xml',
+        'views/bonretour.xml',
+        #'views/stock_inherit.xml',
+        'wizard/create_park.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
